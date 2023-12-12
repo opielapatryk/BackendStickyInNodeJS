f728787c76e0852bd039e4fdea1d83d66122143d
opielapatryk <xpatrykopiela@gmail.com>
2023-12-12 12:04:49 +0100

    register users

--- model/User.js
+++ model/User.js
@@ -1,9 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    friendsRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    isPrivate: { type: Boolean, default: false },
+    askBeforeStick: { type: Boolean, default: false },
+    stickersOnBoard: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Sticker' }],
+    pending: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Sticker' }],
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    password: { type: String, required: true,minlength: 8, },
+    password2: { type: String, required: true,minlength: 8, },
 });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    // Hash password and password2 only if they are modified
+    if (user.isModified('password') || user.isModified('password2')) {
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(user.password, salt);
+        const hashPassword2 = await bcrypt.hash(user.password2, salt);
+
+        user.password = hashPassword;
+        user.password2 = hashPassword2;
+    }
+
+    // Check if password and password2 are the same
+    if (user.isModified('password') && user.isModified('password2') && user.password !== user.password2) {
+        return next(new Error('Password and password2 do not match'));
+    }
+
+    next();
+  });
 
 const User = mongoose.model('User', userSchema);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
-
 const router = express.Router()
 
 const User = require('../model/User');
+
+//Register user
+router.post('/register', async (req, res) => {
+    const { first_name, last_name, email, password, password2 } = req.body;
+    try {
+      const user = new User({ first_name, last_name, email, password,password2 });
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 //Get All Users
 router.get('/get', async (req, res) => {
@@ -23,22 +34,6 @@ router.get('/get/:id', async (req, res) => {
     }
     catch(error){
         res.status(500).json({message: error.message})
-    }
-})
-
-//Post User Method
-router.post('/postUser', async (req, res) => {
-    const data = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name
-    })
-
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
     }
 })
 
